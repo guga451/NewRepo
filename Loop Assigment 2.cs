0262dfef4738b77997c66f5e3b349b452ed22884
guga451 <gugachanukvadze@gmail.com>
2025-09-17 19:39:54 +0400

    Add files via upload

---
+++ Loop Assigment 2.cs
@@ -0,0 +1,158 @@
+﻿//Assignment 1
+
+//namespace Assignment 
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//           for (int i = 1; i <= 10; i++)
+//            {
+//               Console.WriteLine(i);
+//            }
+//            }
+//        }
+//    }
+
+
+
+
+//Assignment 2
+
+//namespace Assignment
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+
+//{
+//    int sum = 0;
+//    for (int i = 1; i <= 100; i++)
+//    {
+//        sum = sum + i;
+//    }
+//    Console.WriteLine($"Sum = {sum}");
+//}
+
+
+
+
+
+//Assignment 3
+
+//namespace Assignment
+//{
+//      internal class Program
+//      {
+//          static void Main(string[] args)
+//          {
+
+
+//for (int i = 1; i <= 19; i = i +2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+
+//Assignment 4
+
+//namespace Assignment
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter 5");
+//            int n = int.Parse(Console.ReadLine());
+
+//            n = 5;
+
+//            for (int i = 1; i <= 9; i++)
+
+//            {
+//                int result = n * i;
+
+//                Console.WriteLine($"{n} * {i} = {result}");
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+//Assignment 5
+
+
+//for (int i = 10; i >= 1; i--)
+//{
+
+//    Console.WriteLine(i);
+//}
+
+
+
+
+
+//Assignment 6
+
+
+//Console.WriteLine("Enter a number to find its factorial:");
+
+//int n = int.Parse(Console.ReadLine());
+
+//int factorial = 1;
+
+//for (int i = 1;  i<=n; i++)
+//{
+//   factorial = factorial * i;
+//}
+//Console.WriteLine($"factorial = {factorial}");
+
+
+
+
+
+//Assignment 7
+
+//char symbol = '*';
+
+//int n= 10;
+
+//string result = "";
+
+//for (int i = 1; i <= n; i++)
+//{
+//    result = result + symbol;
+//}
+//Console.WriteLine(result);
+
+
+
+
+
+//Assignment 8
+
+//int sum = 0;
+//int number = 1;
+
+//while(sum<50)
+//{
+//    sum = sum + number;
+//    number++;
+//}
+//Console.WriteLine($"Sum = {sum}");
+
+
+
+
+
+
+
+
